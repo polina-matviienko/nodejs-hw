19e80088d1773ff1c5764a1205a2396841c0a9e7
Polina Matviienko <polinamatviienko030@gmail.com>
2026-05-22 17:34:17 +0300

    add userId to note model

--- src/models/note.js
+++ src/models/note.js
@@ -18,12 +18,18 @@ const noteSchema = new Schema(
       enum: TAGS,
       default: 'Todo',
     },
+
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
   },
   {
     timestamps: true,
   },
 );
 
-noteSchema.index({ tag: 1 });
+noteSchema.index({ userId: 1, tag: 1 });
 
 export const Note = model('Note', noteSchema);
